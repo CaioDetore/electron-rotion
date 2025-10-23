23e86d50fb5c30c6cad137ffea17e7f593aa8f45
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-10-23 19:20:08 -0300

    feat: editor de texto

--- src/renderer/src/pages/blank.tsx
+++ src/renderer/src/pages/blank.tsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom";
+import { Editor } from '../components/editor';
+import {ToC} from '../components/ToC'
 
 export function Blank() {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      Selecione ou crie um documento
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold text-xs">
+          TABLE OF CONTENTS
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Banco de dados</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className='flex-1 flex flex-col items-center'>
+        <Editor />
+      </section>
     </main>
   );
 }
